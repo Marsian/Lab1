ec62a416211f84ec0ef8feac0006cd6ab50e2da6
pengyuandu <pengyuandu@gmail.com>
2014-01-12 15:40:17 -0800

    script

--- script.sh
+++ script.sh
@@ -1,1 +1,3 @@
-echo "what a" && echo "fucking day"
+echo "what a" && echo "fucking day" || mkdir scripts; cd scripts
+sh sh02.sh | source sh02.sh < test -e /dmtsai $$ echo "exist" || echo "Not exist"
+cd /tmp/vitest ; vim man.config > fucking yanxi | grep 'ass hole'
